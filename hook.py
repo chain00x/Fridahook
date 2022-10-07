d8f18775077e0f5c9d06330712b6a9aca70c93a8
chain00x <90015694+chain00x@users.noreply.github.com>
2022-10-07 15:06:56 +0800

    Update hook.py

--- hook.py
+++ hook.py
@@ -4,13 +4,22 @@ rdev = frida.get_remote_device()
 process = rdev.enumerate_processes()#获取手机所有进程
 session = rdev.attach("InjuredAndroid")
 script_js = """
-let g = Java.use("b.c.a.a.f.g");
-g["l"].implementation = function () {
-    console.log('l is called');
-    let ret = this.l();
-    console.log('l ret value is ' + ret);
+Java.perform(function(){
+
+console.log('hook')
+
+#js代码开始
+let k = Java.use("b3nac.injuredandroid.k");
+k["a"].implementation = function (str) {
+    console.log('a is called' + ', ' + 'str: ' + str);
+    let ret = this.a(str);
+    console.log('a ret value is ' + ret);
     return ret;
 };
+#js代码结束
+
+}
+)
 """
 def on_message(message, data):
 	if message["type"] == "send":
